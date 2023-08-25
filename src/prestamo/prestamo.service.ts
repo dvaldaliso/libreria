af1d0b9aa301725ce038263c0f574e8e339cd7e3
David Valdaliso Lopez <dvaldaliso@gmail.com>
2023-08-25 19:32:30 -0400

    adicionando transaction en el prestamo

--- src/prestamo/prestamo.service.ts
+++ src/prestamo/prestamo.service.ts
@@ -2,8 +2,8 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreatePrestamoDto } from './dto/create-prestamo.dto';
 import { UpdatePrestamoDto } from './dto/update-prestamo.dto';
 import { Prestamo } from './entities/prestamo.entity';
-import sequelize, { Sequelize } from 'sequelize';
 import { Libro } from '../libro/libro.entity';
+import { Sequelize } from 'sequelize';
 
 
 @Injectable()
@@ -15,42 +15,50 @@ export class PrestamoService {
 
     @Inject('LIBROS_REPOSITORY')
     private libroRepository: typeof Libro,
-
+    
+    @Inject('SEQUELIZE')
+    private sequelize: Sequelize
    
   ) {}
-  //private sequelize: Sequelize;
+  
 
   async create(createPrestamoDto: CreatePrestamoDto):Promise<string> {
-    //const t = await this.sequelize.transaction()
-    const libroPrestado = await this.libroRepository.findOne({
-      where: {
-        id: createPrestamoDto.libroId,
-        estado: 1,
-      }/*,
-      transaction: t,*/
-    });
-
-    if (libroPrestado) {
-      throw new Error('El libro ya está prestado');
-    }
-
-    /*const prestamo = await this.prestamoRepository.create({...createPrestamoDto}, {
-      transaction: t,
-    });*/
-    const prestamo = await this.prestamoRepository.create({...createPrestamoDto});
-
-    await this.libroRepository.update({
-      estado: 1,
-    }, {
-      where: {
-        id: createPrestamoDto.libroId,
+    const t = await this.sequelize.transaction()
+    try {
+     
+      const libroPrestado = await this.libroRepository.findOne({
+        where: {
+          id: createPrestamoDto.libroId,
+          estado: 1,
+        },
+        transaction: t,
+      });
+  
+      if (libroPrestado) {
+        throw new Error('El libro ya está prestado');
       }
-    });
-
-    // Confirma la transacción
-    //await t.commit();
-
-    return `se ha relizado un prestamo del libro `
+  
+      const prestamo = await this.prestamoRepository.create({...createPrestamoDto}, {
+         transaction:t,
+      });
+      //const prestamo = await this.prestamoRepository.create({...createPrestamoDto});
+  
+      await this.libroRepository.update({
+        estado: 1,
+      }, {
+        where: {
+          id: createPrestamoDto.libroId,
+        } ,transaction:t   
+      });
+  
+      // Confirma la transacción
+      await t.commit();
+      return `se ha relizado un prestamo del libro `
+    } catch (error) {
+      await t.rollback();
+      return `no se ha relizado un prestamo del libro ${error}`
+    }
+   
   }
 
  
